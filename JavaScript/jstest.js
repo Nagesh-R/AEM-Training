18531b4bdd61a5dfb8b5b401552d2c8b999a7170
ICF2025845 <icf2025845@ICF2025845.local>
2019-06-19 15:53:31 +0530

    javascript

---
+++ JavaScript/jstest.js
@@ -0,0 +1,11 @@
+var car = {
+    color : "blue",
+    speed : 300,
+    engine : [
+        {make: "bmw"},
+        {size : 3.0},
+        {fuel : "petrol"},
+        {capacity:6.0+" ltr"}],
+    drive:function(){return "drive";}
+    
+};
---
+++ JavaScript/one.js
@@ -0,0 +1,8 @@
+var person = {
+  firstName: "milackron",
+  lastName : "tg",
+  id       : 47,
+  fullName : function() {
+    return this.firstName + " " + this.lastName+" "+this.id;
+  }
+};
